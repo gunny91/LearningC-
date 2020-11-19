450c59d60b56e859ce06e7e7b651e189afc4a8ac
Geonhee Cho <gunny91@iastate.edu>
2020-11-19 16:57:51 +0900

    array in c sharp

---
+++ ArrayPractice/ArrayPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] cars = { "BMW", "KIA", "HYUNDAI", "FORD" };
+
+            int[] num = { 1, 2, 3, 4 };
+
+
+            for (int i = 0; i < cars.Length; i++) {
+                Console.WriteLine(cars[i]);
+                Console.WriteLine(num[i]);
+            }
+
+            Console.WriteLine("/////////////////////////////////");
+            foreach (string i in cars) { Console.WriteLine(i); }
+        }
+    }
+}
