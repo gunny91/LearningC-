9a9c082bb9df8ece1a334eb23018ecec6d720a86
Geonhee Cho <gunny91@iastate.edu>
2020-11-19 17:08:47 +0900

    method practice

---
+++ Method/Method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Method
+{
+    
+    
+    class Program
+    {
+
+        static void MyMethod() { Console.WriteLine("First method in C #"); }
+
+        static void ParaMthod(string fname) { Console.WriteLine(fname+" Refsnes"); }
+
+        static void GetCountry(string country = "Korea") { Console.WriteLine(country); }
+
+        static int AdditionHundred(int x) { return x + 100; }
+
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+
+            ParaMthod("Hello");
+            ParaMthod("Gunny");
+            ParaMthod("Yi Ting");
+
+            GetCountry("USA");
+            GetCountry();
+
+            Console.WriteLine( AdditionHundred(100000));
+
+        }
+    }
+}
