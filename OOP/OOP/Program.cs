7709ce3de57efd99c691898c5f31a679d3d81246
Geonhee Cho <gunny91@iastate.edu>
2020-11-19 17:24:45 +0900

    Learning OOP

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP
+{
+
+     class Car
+    {
+        public string color = "red";
+    }
+        class Program
+    {
+        String color = "red";
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            Console.WriteLine(car.color);
+        }
+    }
+}
