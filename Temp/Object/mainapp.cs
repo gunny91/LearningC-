ff7a3991b368e887d180af2ef7c5a333a32576ab
Geonhee Cho <gunny>
2020-11-30 22:16:59 +0900

    update object practice

---
+++ Temp/Object/mainapp.cs
@@ -0,0 +1,22 @@
+﻿/*using System;
+
+namespace Object
+{
+    class mainapp
+    {
+        static void Main(string[] args) 
+        {
+            object a = 123;
+            object b = 3.1415123124241241m;
+            object c = true;
+            object d = "hello";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+        }
+    }
+}
+*/
---
+++ Temp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            object a = 123;
+            object b = 3.1415123124241241m;
+            object c = true;
+            object d = "hello";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
